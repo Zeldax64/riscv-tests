c0d3cf52df94aa281a8bf827745259d3db0478cf
Zelda <caioxyz@hotmail.com>
2019-07-15 19:22:13 -0300

    Added mt-conv2d benchmark!
    A new benchmark was added. This benchmark performs a sobel operation on a image.

---
+++ benchmarks/mt-conv2d/conv2d.c
@@ -0,0 +1,44 @@
+
+// See LICENSE for license details.
+#include <stdio.h>
+#include <stdlib.h>
+#include "dataset.h"
+
+short sum(data_t_s window[KERNEL_DIM][KERNEL_DIM]){
+    short valOutput = 0;
+    for(int idxRow = 0;idxRow < KERNEL_DIM;idxRow++) {
+        for(int idxCol = 0; idxCol < KERNEL_DIM; idxCol++) {
+            valOutput = valOutput + window[idxRow][idxCol];
+        }
+    }
+    return valOutput;
+}
+
+void conv2d(int coreid, int ncores, int img_height,int img_width,int kernel_dim, data_t img[IMG_HEIGHT][IMG_WIDTH],data_t_s kernel[KERNEL_DIM*KERNEL_DIM], data_t result[IMG_HEIGHT][IMG_WIDTH])
+{
+    data_t_s window[kernel_dim][kernel_dim];
+    int i;int j;
+    short valOutput;
+    i=coreid;
+    while(i< img_height){
+        for(j=0;j<img_width;j++){
+            if((i>=kernel_dim-1)&&(j>=kernel_dim-1)&&(i<=img_height-kernel_dim)&&(j<=img_width-kernel_dim)){
+                for(int idxWinRow = 0; idxWinRow<kernel_dim; idxWinRow++){
+                    for(int idxWinCol = 0; idxWinCol<kernel_dim; idxWinCol++){
+                        short val = (short) img[i-1+idxWinRow][j-1+idxWinCol];
+                        val = (short) kernel[(idxWinRow*kernel_dim)+idxWinCol] * val;
+                        window[idxWinRow][idxWinCol]=val;
+                    }
+                }
+                valOutput = sum(window);
+                if(valOutput<0)
+                    valOutput=0;
+                result[i][j]=valOutput;
+            }
+            else
+                result[i][j]=0;
+        }
+        i=i+ncores;
+    }
+
+}
---
+++ benchmarks/mt-conv2d/conv2d_gendata.py
@@ -0,0 +1,55 @@
+def img():
+	import numpy as np
+	import cv2
+	from matplotlib import pyplot as plt
+	img=cv2.imread('imgBig_lena.bmp',0);
+	height, width = img.shape
+	arq=open('img2.h','w')
+	bufaux="unsigned char image["+str(height)+"]["+str(width)+"] = {\n"
+	arq.write(bufaux)
+
+	for x in range(height):
+		arq.write("{")
+		for y in range(width):
+			buf= "%d" %(img[x][y])
+			if(y!=width-1):
+				buf=buf+","
+			arq.write(buf)
+		arq.write("}")
+		if(x!=height-1):
+			arq.write(",")
+			arq.write("\n")
+	bufaux="};"
+	arq.write(bufaux)
+
+	arq.close()
+def result():
+	import numpy as np
+	import cv2
+	from matplotlib import pyplot as plt
+	img2=cv2.imread('test.png',0);
+	height, width = img2.shape
+	arq=open('resultsobel.h','w')
+	bufaux="unsigned char resultimage["+str(height)+"]["+str(width)+"] = {\n"
+	arq.write(bufaux)
+
+	for x in range(height):
+		arq.write("{")
+		for y in range(width):
+			buf= "%d" %(img2[x][y])
+			if(y!=width-1):
+				buf=buf+","
+			arq.write(buf)
+		arq.write("}")
+		if(x!=height-1):
+			arq.write(",")
+			arq.write("\n")
+	bufaux="};"
+	arq.write(bufaux)
+
+	arq.close()
+
+def main():
+	img()
+	result()
+main()
